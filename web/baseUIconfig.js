c5fd1124a7b5f925bb80ce5f2ef4366b19aec938
matjvs <34428216+matjvs@users.noreply.github.com>
2018-10-21 14:44:13 +0200

    gekko bot

--- web/baseUIconfig.js
+++ web/baseUIconfig.js
@@ -4,10 +4,10 @@
 // This config is used by both the frontend as well as the web server.
 // see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
 
-const CONFIG = {
-  headless: false,
+var UIconfig = require('../vue/statics/UiConfig');
+  headless: true,
   api: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 3000,
     timeout: 120000 // 2 minutes
   },
--- web/vue/dist/UIconfig.js
+++ web/vue/dist/UIconfig.js
@@ -5,9 +5,9 @@
 // see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
 
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 3000,
     timeout: 120000 // 2 minutes
   },
